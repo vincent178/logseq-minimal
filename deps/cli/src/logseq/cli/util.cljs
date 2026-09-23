71ab6509d432c7f61c7fe9bc6a8f29768c776692
vincent178 <vh7157@gmail.com>
2026-09-24 04:34:08 +0800

    fix(3e): clean deps/cli dead code + carve ignores for shared common fns
    - deps/cli/.carve/ignore: exempt cli.common fns referenced from src/main
      (deps/cli carve can't see cross-dir call-sites); drop deleted command entries
    - deps/cli/util.cljs: delete dead get-graph-path, summarize-build-edn,
      ensure-db-graph-for-command, api-command?, ->open-db-args + 5 unused requires

--- deps/cli/src/logseq/cli/util.cljs
+++ deps/cli/src/logseq/cli/util.cljs
@@ -1,29 +1,8 @@
 (ns ^:node-only logseq.cli.util
   "CLI only util fns"
-  (:require ["fs" :as fs]
-            ["path" :as node-path]
-            [clojure.string :as string]
-            [logseq.cli.common.graph :as cli-common-graph]
-            [logseq.db.common.entity-plus :as entity-plus]
-            [logseq.db.common.sqlite :as common-sqlite]
+  (:require [clojure.string :as string]
             [nbb.error]
             [promesa.core :as p]))
-
-(defn ->open-db-args
-  "Creates args for sqlite-cli/open-db! given a graph. Similar to sqlite-cli/->open-db-args"
-  [graph]
-  (cond
-    (and (fs/existsSync graph) (.isFile (fs/statSync graph)))
-    [graph]
-    (string/includes? graph "/")
-    ((juxt node-path/dirname node-path/basename) graph)
-    :else
-    [(cli-common-graph/get-db-graphs-dir) (common-sqlite/sanitize-db-name graph)]))
-
-(defn get-graph-path
-  "If graph is a file, return its path. Otherwise returns the graph's dir"
-  [graph]
-  (apply node-path/join (->open-db-args graph)))
 
 (defn api-fetch [token method args]
   (js/fetch "http://127.0.0.1:12315/api"
@@ -69,28 +48,3 @@
   [& strings]
   (apply println "Error:" strings)
   (js/process.exit 1))
-
-(defn summarize-build-edn
-  "Given a sqlite.build EDN map, returns a string summarizing what is transacted"
-  [edn-map]
-  (let [pluralize (fn pluralize [word num]
-                    (if (= 1 num)
-                      word
-                      (get {"property" "properties"
-                            "class" "classes"} word (str word "s"))))]
-    (str (count (:properties edn-map)) " " (pluralize "property" (count (:properties edn-map))) ", "
-         (count (:classes edn-map)) " " (pluralize "class" (count (:classes edn-map))) " and "
-         (count (:pages-and-blocks edn-map)) " " (pluralize "page" (count (:pages-and-blocks edn-map))))))
-
-(defn ensure-db-graph-for-command
-  [db]
-  (when-not (entity-plus/db-based-graph? db)
-    (error "This command must be called on a DB graph")))
-
-(defn api-command?
-  "Given user options and $LOGSEQ_API_SERVER_TOKEN, determines if
-   given command is an api (true) or local (false) command"
-  [{:keys [graph graphs api-server-token]}]
-  (or api-server-token
-      ;; graph(s) check overrides env since it is more explicit
-      (and js/process.env.LOGSEQ_API_SERVER_TOKEN (not graph) (not graphs))))
